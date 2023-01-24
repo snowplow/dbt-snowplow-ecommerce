563d7752a2626992bc62af23e3ba63ef9bff7588
emielver <emiel@snowplowanalytics.com>
2023-01-24 14:38:12 +0000

    Add support for Databricks (Close #7)

--- models/sessions/snowplow_ecommerce_sessions.sql
+++ models/sessions/snowplow_ecommerce_sessions.sql
@@ -7,12 +7,18 @@
     partition_by = snowplow_utils.get_partition_by(bigquery_partition_by = {
       "field": "start_tstamp",
       "data_type": "timestamp"
-    }),
-    tags=["derived"]
+    }, databricks_partition_by='start_tstamp_date'),
+    tags=["derived"],
+    tblproperties={
+      'delta.autoOptimize.optimizeWrite' : 'true',
+      'delta.autoOptimize.autoCompact' : 'true'
+    }
   )
 }}
 
 select *
-
+  {% if target.type in ['databricks', 'spark'] -%}
+  , DATE(start_tstamp) as start_tstamp_date
+  {%- endif %}
 from {{ ref('snowplow_ecommerce_sessions_this_run') }}
 where {{ snowplow_utils.is_run_with_new_events('snowplow_ecommerce') }}
