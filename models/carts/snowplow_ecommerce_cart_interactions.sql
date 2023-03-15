93aa5c888ac5e82bb5b6c40c76338861d49b9795
Ryan Hill <r.l.hill128@gmail.com>
2023-03-15 14:59:55 +0000

    Migrate to new mat

--- models/carts/snowplow_ecommerce_cart_interactions.sql
+++ models/carts/snowplow_ecommerce_cart_interactions.sql
@@ -1,13 +1,13 @@
 {{
   config(
-    materialized=var("snowplow__incremental_materialization"),
+    materialized="incremental",
     unique_key='event_id',
     upsert_date_key='derived_tstamp',
     sql_header=snowplow_utils.set_query_tag(var('snowplow__query_tag', 'snowplow_dbt')),
-    partition_by = snowplow_utils.get_partition_by(bigquery_partition_by = {
+    partition_by = snowplow_utils.get_value_by_target_type(bigquery_val = {
       "field": "derived_tstamp",
       "data_type": "timestamp"
-    }, databricks_partition_by='derived_tstamp_date'),
+    }, databricks_val='derived_tstamp_date'),
     tags=["derived"],
     tblproperties={
       'delta.autoOptimize.optimizeWrite' : 'true',
