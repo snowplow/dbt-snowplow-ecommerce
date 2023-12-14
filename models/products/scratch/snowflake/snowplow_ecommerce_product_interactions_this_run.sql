64bb17222b39d0621977cb99b024c09ecd316b46
Emiel <emiel.verkade@gmail.com>
2023-12-14 15:19:25 +0000

    Add `ecommerce_action_type` and `ecommerce_action_name` to `product_interactions` (Close #39)

--- models/products/scratch/snowflake/snowplow_ecommerce_product_interactions_this_run.sql
+++ models/products/scratch/snowflake/snowplow_ecommerce_product_interactions_this_run.sql
@@ -83,6 +83,10 @@ select
   derived_tstamp,
   derived_tstamp_date,
 
+  -- ecommerce action fields
+  ecommerce_action_type,
+  ecommerce_action_name,
+
   -- ecommerce product fields
   product_id,
   product_category,
