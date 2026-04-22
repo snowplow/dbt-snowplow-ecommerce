1717fa51ad87addbaa55b639cb032e4dcdbaa2fe
Greg Leonard <45019882+greg-el@users.noreply.github.com>
2026-04-22 12:33:05 +0100

    Update for dbt 1.10

--- models/base/manifest/snowplow_ecommerce_base_sessions_lifecycle_manifest.sql
+++ models/base/manifest/snowplow_ecommerce_base_sessions_lifecycle_manifest.sql
@@ -5,26 +5,23 @@ and you may not use this file except in compliance with the Snowplow Personal an
 You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 at https://docs.snowplow.io/personal-and-academic-license-1.0/
 #}
 
-{{
-    config(
-        materialized="incremental",
-        unique_key='session_identifier',
-        upsert_date_key='start_tstamp',
-        partition_by = snowplow_utils.get_value_by_target_type(bigquery_val={
+{{ config(
+    materialized="incremental", 
+    unique_key='session_identifier', 
+    partition_by=snowplow_utils.get_value_by_target_type(bigquery_val={
             "field": "start_tstamp",
             "data_type": "timestamp"
-        }, databricks_val='start_tstamp_date'),
-        cluster_by=snowplow_utils.get_value_by_target_type(bigquery_val=["session_identifier"], snowflake_val=["to_date(start_tstamp)"]),
-        full_refresh=snowplow_ecommerce.allow_refresh(),
-        tags=["manifest"],
-        sql_header=snowplow_utils.set_query_tag(var('snowplow__query_tag', 'snowplow_dbt')),
-        tblproperties={
+        }, databricks_val='start_tstamp_date'), 
+    cluster_by=snowplow_utils.get_value_by_target_type(bigquery_val=["session_identifier"], snowflake_val=["to_date(start_tstamp)"]), 
+    full_refresh=snowplow_ecommerce.allow_refresh(), 
+    tags=["manifest"], 
+    sql_header=snowplow_utils.set_query_tag(var('snowplow__query_tag', 'snowplow_dbt')), 
+    tblproperties={
             'delta.autoOptimize.optimizeWrite' : 'true',
             'delta.autoOptimize.autoCompact' : 'true'
-        },
-        snowplow_optimize=true
-    )
-}}
+        }, 
+    meta={'upsert_date_key': 'start_tstamp', 'snowplow_optimize': true}
+) }}
 
 {% set sessions_lifecycle_manifest_query = snowplow_utils.base_create_snowplow_sessions_lifecycle_manifest(
     session_identifiers=session_identifiers(),
